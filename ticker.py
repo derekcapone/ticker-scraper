1299bd5ceb2e8cea40ef683dc794806d9a31cbae
Derek Capone <derekcapone7@gmail.com>
2021-03-18 21:40:50 -0300

    Remove any use of json file to store company data, and remove unused functions in ticker.py

--- ticker.py
+++ ticker.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import logging
 import re
 import datetime
@@ -26,10 +25,10 @@ class TickerInfo:
         self.valid_ticker = False
         self.date_last_accessed = datetime.date.today()
         # TODO: once more details about seen companies are stored, add those details here
-        name = is_ticker_seen(self.ticker)
+        name = get_existing_company(self.ticker)
         if name != None:
             # Valid stock ticker we've already seen
-            self.company_name = name
+            self.company_name = name['company_name']
             self.company_seen = True
             self.valid_ticker = True
             return
@@ -39,7 +38,6 @@ class TickerInfo:
             self.company_name = name
             self.valid_ticker = True
 
-            update_seen_tickers({self.ticker: self.company_name})
         return
 
     def generate_company_dict(self):
@@ -79,51 +77,6 @@ def insert_company_info(company: TickerInfo):
     mng.insert_single_document(company.generate_company_dict())
 
 
-def is_ticker_seen(ticker):
-    """
-    Checks for whether the ticker has already been tracked
-    :param ticker:
-    :return: String containing company name if ticker already seen, None otherwise
-    """
-    logging.info("Checking if {} already in list".format(ticker))
-    with open("seen_tickers.json", "r+") as file:
-        data = json.load(file)
-        try:
-            company_name = data[ticker]
-        except KeyError:
-            company_name = None
-        finally:
-            return company_name
-
-
-def update_seen_tickers(new_ticker: dict):
-    """
-    Updates seen_tickers.json with new ticker
-    :param new_ticker: dict holding ticker and company name: {"ticker": "company name"}
-    """
-    logging.info("Adding {} to ticker list".format(next(iter(new_ticker))))
-    with open("seen_tickers.json", "r+") as file:
-        data = json.load(file)
-        data.update(new_ticker)
-        file.seek(0)
-        json.dump(data, file, indent=4)
-
-
-def add_new_ticker(ticker: str):
-    """
-    Checks if ticker corresponds to actual company, adds to seen_tickers.json if so, discard otherwise
-    :param ticker: String holding possible ticker
-    :return:
-    """
-    company = get_company_name(ticker)
-
-    if company is not None:
-        new_company = {ticker: company}
-        update_seen_tickers(new_company)
-    else:
-        logging.info("{} is not valid ticker".format(ticker))
-
-
 def is_possible_ticker(tick_str: str):
     """
     Parse tick_str to check if string is between 1 and 6 characters and is uppercase (structure of ticker symbols)
@@ -154,7 +107,7 @@ def check_comment_str(comment: str):
     return ticker_list
 
 
-def parse_possible_tickers(ticker_list: list):
+def parse_possible_tickers(ticker_list: list, active_companies):
     """
     Goes through ticker_list and checks each string to see if it's a valid ticker
     If ticker is valid, checks seen tickers for this, adds to seen tickers if valid
@@ -167,9 +120,12 @@ def parse_possible_tickers(ticker_list: list):
             raise TypeError("Given object is not a string: {} is {}".format(ticker, type(ticker)))
             return
 
-        new_company = TickerInfo(ticker)
-        if new_company.valid_ticker == True:
-            company_list.append(new_company)
+        if not any(active.ticker == ticker for active in active_companies):
+            new_company = TickerInfo(ticker)
+            if new_company.valid_ticker:
+                company_list.append(new_company)
+        else:
+            logging.info("{} already in active companies.".format(ticker))
 
     return company_list
 
@@ -203,19 +159,19 @@ def delete_all_company_data():
 
 def test_add_tickers_from_string():
     """
-    Test function display how the adding company info to the
-    :return:
+    Test function to display how this module can be used to parse strings, query database, hold active company objects etc.
     """
+    active_companies = []
     test_str = "this ADC. string ADM, contains CEO! AMOV a FCAC possible FCST ticker"
 
     list_of_tickers = check_comment_str(test_str)
-    active_companies = parse_possible_tickers(list_of_tickers)
+    active_companies += parse_possible_tickers(list_of_tickers, active_companies)
 
     for company in active_companies:
-        insert_company_info(company)
+        # insert company info to database if this is the first time we've seen this company
+        if not company.company_seen:
+            insert_company_info(company)
 
 
 if __name__ == "__main__":
-    ticker = "ADC"
-    active = get_existing_company(ticker)
-    print(active)
+    test_add_tickers_from_string()
